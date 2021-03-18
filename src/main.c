a3553015abbd8636559528bc09d16b3fc53a206e
icaro156 <icaro1561@outlook.com>
2021-03-17 21:27:56 -0300

    testa ai rico

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 #include"../lib/ponto.h"
 #include"../lib/entradas.h"
@@ -21,35 +22,51 @@ int main(int argc, char** argv){
   capsulaUf*** matriz;
   int *tamanhos = malloc(k*sizeof(int));
   //le as entradas do arquivo e retorna 
+  clock_t tempo = clock();
   pontos = leEntradas(file,pontos);
   int i=0;
-
   quantCoordenadas = contaCoordenada(file);
   quantLinhas = contaLinhas(file);
-
+  
+  tempo = clock() - tempo;
+  double tempoEntradas = ((double)tempo) / CLOCKS_PER_SEC;
+  
+  tempo = clock();
   distancias = calculaDistancias(pontos,quantLinhas,quantCoordenadas);
-
+  tempo = clock() - tempo;
+  double tempoCalcDist = ((double)tempo) / CLOCKS_PER_SEC;
+  
+  tempo = clock();
   int tamanho = ((quantLinhas*(quantLinhas - 1))/2 );
   qsort(distancias,tamanho,sizeof(distancia*),sort);
-
-
+  tempo = clock()- tempo;
+  double tempoOrdDist = ((double)tempo) / CLOCKS_PER_SEC;
+  
+  tempo = clock();
   vetorUf = UF_init(quantLinhas, pontos);
   int limite = quantLinhas - k; //pra tirar as k-1 arestas;
   kruskall(vetorUf,tamanho,distancias,limite);
+  tempo = clock() - tempo;
+  double tempokruskall = ((double)tempo) / CLOCKS_PER_SEC;
+
   destroiDistancias(distancias,quantLinhas,quantCoordenadas);
-  // tiraK(vetorUf,k,quantLinhas);
   int tamanhoSemK= quantLinhas-k;
+  
+  tempo = clock();
   matriz = testeSort(vetorUf,quantLinhas, k, tamanhos);
+  double temposaida = ((double)tempo) / CLOCKS_PER_SEC;
   destroiUf(vetorUf, quantLinhas);
   escreveSaida(matriz, tamanhos, saida, k);
-  
+  tempo = clock() -  tempo;
+
   for(int i=0; i< quantLinhas; i++){
     destroiPonto(pontos[i]);
   }
 
   
-  destroiMatriz(matriz,tamanhos,k);
+  printf("Tempo entradas: %lf  | Tempo calculando distancia: %lf | Tempo ordenando distancia: %lf | tempo kruskall: %lf | tempo saida: %lf",tempoEntradas,tempoCalcDist,tempoOrdDist,tempokruskall,temposaida);
   
+  destroiMatriz(matriz,tamanhos,k);
   free(tamanhos);
   free(pontos);
   fclose(file);
